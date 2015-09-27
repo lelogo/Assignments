290d75325748aa9881ad3ff9480a7028d20852fe
Neil <Neil@172.31.47.103>
2015-09-27 11:50:53 -0400

    corrected gitignore in assignments/src

---
+++ Assignments/src/Intersect.java
@@ -0,0 +1,8 @@
+
+public class Intersect {
+	public static void main(String[] args){
+		
+		
+	}
+
+}
---
+++ Assignments/src/Sum.java
@@ -0,0 +1,14 @@
+
+public class Sum {
+	
+	public static void main(String[] args){
+		
+		System.out.println("enter an integer");
+		int a=IO.readInt();
+		System.out.println("enter another");
+		int b=IO.readInt();
+		IO.outputIntAnswer(a + b);
+
+	}
+
+}
